cf422b2b87c28204b1d97b2974c5625fdebb39e4
Richard Garcia <richardgarcia892@gmail.com>
2021-10-31 01:05:18 -0400

    fix

--- index.js
+++ index.js
@@ -1,42 +1,38 @@
-const express = require('express');
-const cors = require('cors');
-const routerApi = require('./routes');
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const routerApi = require('./routes')
 
 
-const { logError, errorHandler, boomErrorHandled } = require('./middleware/error.handler');
+const { logError, errorHandler, boomErrorHandled } = require('./middleware/error.handler')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use(helmet())
 
-const corsWhitelist = ['http://localhost:8080', 'https://myapp.co'];
+const corsWhitelist = ['http://localhost:8080', 'https://myapp.co']
 const corsOptions = {
   origin: (origin, callback) => {
     if (corsWhitelist.includes(origin) || !origin) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error('no permitido'));
+      callback(new Error('no permitido'))
     }
   }
 }
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
-app.get('/', (req, res) => {
-  res.send('Hola mi server en express');
-});
-
-app.get('/nueva-ruta', (req, res) => {
-  res.send('Hola, soy una nueva ruta');
-});
-
-routerApi(app);
+routerApi(app)
 
 app.use(logError);
-app.use(boomErrorHandled);
-app.use(errorHandler);
+app.use(boomErrorHandled)
+app.use(errorHandler)
 
 
 app.listen(port, () => {
-  console.log('Mi port' + port);
+  console.log(`App running on port ${port}`)
 });
